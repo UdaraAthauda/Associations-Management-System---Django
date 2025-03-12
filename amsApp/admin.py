4ae5a053ec3de17650335793b5e7ef0ba6e52b38
UdaraAthauda <athaudaprasath@gmail.com>
2025-03-12 12:31:45 +0530

    admin dashboard table filtering and permission groups creation

--- amsApp/admin.py
+++ amsApp/admin.py
@@ -9,9 +9,26 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ['id', 'username']
     search_fields = ['id', 'username']
 
+@admin.register(Association)
+class AssociationAdmin(admin.ModelAdmin):
+    search_fields = ['AssociationName']
 
-admin.site.register(Association)
-admin.site.register(AssociationMember)
-admin.site.register(Service)
-admin.site.register(ServiceRequest)
-admin.site.register(UserFeedbacks)
+@admin.register(AssociationMember)
+class AssociationMemberAdmin(admin.ModelAdmin):
+    search_fields = ['id', 'user__username', 'association__AssociationName']
+    list_filter = ['adminAccept']
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    search_fields = ['serviceTitle', 'association__AssociationName']
+
+@admin.register(ServiceRequest)
+class ServiceRequestAdmin(admin.ModelAdmin):
+    search_fields = ['user__username', 'service__association', 'service__serviceTitle']
+    list_filter = ['status']
+
+@admin.register(UserFeedbacks)
+class UserFeedbacksAdmin(admin.ModelAdmin):
+    list_filter = ['association__AssociationName', 'reply']
+
+
